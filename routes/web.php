85f34c791ff7e5c3d6b8e77c6db6804a06ed5d5a
LordPenguMx <66186837+LordPenguMx@users.noreply.github.com>
2020-10-20 19:22:16 -0500

    Rutas
    Creacion de rutas para cada apartado

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+/*Rutas Admiciones */
 Route::get('/Admiciones/Administracion' , 'NavegadorControlle@Administracion');
